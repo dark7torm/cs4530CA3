bfe69a2d2b09bbb44d83ad383900abdfa9565f40
Ren Taguchi <132172062+dark7torm@users.noreply.github.com>
2024-09-17 14:42:38 -0400

    followed instructions, now workign on after todos

--- src/models/calculator.model.ts
+++ src/models/calculator.model.ts
@@ -5,21 +5,31 @@ import { OperatorKeys } from '../enums/operator-keys.enum';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
 
 export class CalculatorModel implements ICalculatorModel {
+  
+  private _buffer: string = '';
 
   public pressNumericKey(key: NumericKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer += key;
   }
 
   public pressOperatorKey(key: OperatorKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer += key;
   }
 
   public pressActionKey(key: ActionKeys): void {
-    throw new Error('Method not implemented.');
+    if (key === ActionKeys.CLEAR) {
+      this._buffer = '';
+    } 
+    if (key === ActionKeys.DOT) {
+      this._buffer += key;
+    } 
+    if (key === ActionKeys.EQUALS) {
+      this._buffer += key;
+    } 
   }
 
   public display(): string {
-    throw new Error('Method not implemented.');
+    return this._buffer;
   }
 
 }
